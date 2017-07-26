feb3f2529c28b3f5dc6f5f9208fb6cf5902096b0
Alex Miller <alexander.dean.miller@gmail.com>
2017-07-26 01:44:06 +0100

    big style overhaul and work on charts, coming together nicely

--- static/js/graph_2.js
+++ static/js/graph_2.js
@@ -5,17 +5,86 @@ queue()
 function makeGraphs(error, projectsJson) {
 
 
-    //Clean projectsJson data
+    // Clean projectsJson data
     var mhEmployment = projectsJson;
+    //
+    // var dateFormat = d3.time.format("%Y-%m-%d");
+    // mhEmployment.forEach(function (d) {
+    //     d["Year"] = dateFormat.parse(d["Year"] + "-1-1");            //  Torn between keeping date format or not
+    //     d["Year"].setDate(1);
+    // });
 
-    var dateFormat = d3.time.format("%Y-%m-%d");
-    mhEmployment.forEach(function (d) {
-        d["Year"] = dateFormat.parse(d["Year"] + "-1-1");
-        d["Year"].setDate(1);
-       // d["Employment rate of people with mental illness"] = +d["Employment rate of people with mental illness"]; //added this line in - may not need - commented out for now
-    });
+
+
+    // //pie experiment
+    // mhEmployment.forEach(function (d) {
+    //     d.gender = Gender(d.Gender);
+    //     d.total = d.Gender + d.Region + d.Age;       //come back to this - onto something
+    //     d.Year = d.date.getFullYear();
+    // })
+    // //pie experiment
+
+
 
     var ndx = crossfilter(mhEmployment);
+
+    // new functions
+
+    function reduceAdd(p, v) {
+        ++p.count;
+        p.total += v["Employment rate of people with mental illness"];
+        p.average = p.total / p.count;
+        return p;
+}
+
+function reduceRemove(p, v) {
+        --p.count;
+        p.total += v["Employment rate of people with mental illness"];
+        p.average = p.total / p.count;
+        return p;
+}
+
+function reduceInitial() {
+        return {count: 0, total: 0, average: 0};
+}
+
+   function reduceAddGen(p, v) {
+        ++p.count;
+        p.total += v["Employment rate of population"];
+        p.average = p.total / p.count;
+        return p;
+}
+
+function reduceRemoveGen(p, v) {
+        --p.count;
+        p.total += v["Employment rate of population"];
+        p.average = p.total / p.count;
+        return p;
+}
+
+function reduceInitialGen() {
+        return {count: 0, total: 0, average: 0};
+        }
+
+        // functions as above but for pie
+//
+//     function reduceAddGender(p, v) {
+//         ++p.count;
+//         p.total += v["Gender"];
+//         p.average = p.total / p.count;
+//         return p;
+// }
+//
+// function reduceRemoveGender(p, v) {
+//         --p.count;
+//         p.total += v["Gender"];
+//         p.average = p.total / p.count;
+//         return p;
+// }
+//
+// function reduceInitialGender() {
+//         return {count: 0, total: 0, average: 0};
+// }
 
 
     //Dimensions go here
@@ -32,50 +101,148 @@ function makeGraphs(error, projectsJson) {
        return d["Employment rate of population"];
    });
 
+   var genderDim = ndx.dimension(function (d) {
+       return d["Gender"];
+   });
+
+   var regionDim = ndx.dimension(function (d) {
+       return d["Region"];
+   });
+
+      var ageDim = ndx.dimension(function (d) {
+       return d["Age"];
+   });
 
    // Groups go here
 
     var numYear = YearDim.group();
-    var numMhEmployed = mhEmployedDim.group();
-    var numPopEmployed = popEmployedDim.group();
+ //   var numMhEmployed = mhEmployedDim.group();
+ //    var numMhEmployed = YearDim.group().reduceSum(function (d) {
+ //        return d["Employment rate of people with mental illness"];
+  //   });
+    var numMhEmployed = YearDim.group().reduce(reduceAdd, reduceRemove, reduceInitial);
+    var numPopEmployed = YearDim.group().reduce(reduceAddGen, reduceRemoveGen, reduceInitialGen);
 
+    var numGender = genderDim.group();   // was YearDim
+    var numRegion = regionDim.group();
+    var numAge = ageDim.group();
+
+    // var numRegion = YearDim.group().reduce(reduceAdd, reduceRemove, reduceInitial);
+    // var numAge = YearDim.group().reduce(reduceAdd, reduceRemove, reduceInitial);
+
+
+
+//     // remove comma experiment
+//
+//     var array =  [ "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"  ];
+//
+//
+//
+// //Removes the comma from each string
+// array.forEach(function(string, i){
+//    array[i] = string.replace(',', '');
+// });
+//
+//
+// //Output the whole array
+// for(var i = array.length; i--;)
+//    print(array[i]);
+//
+//
+//
+//
+//
+// //Sends things to the DOM
+// function print(text){
+//    var elem = document.createElement('p');
+//    elem.innerHTML = text;
+//    document.body.appendChild(elem);
+// }
+
+
+
+
+
+
+
+    // remove comma experiment
 
     //Chart definitions go here
 
     var minYear = YearDim.bottom(1)[0]["Year"];
     var maxYear = YearDim.top(1)[0]["Year"];
 
-    var YearChart = dc.lineChart("#Year-chart");  // need to change to line chart and experiment with num
+    var YearChart = dc.compositeChart("#Year-chart");  // was #Year-chart
     var MhEmployedChart = dc.rowChart("#mhEmployed-row-chart");
     var PopEmployedChart = dc.rowChart("#popEmployed-row-chart");  // manipulate css to be my own
+    var regionPieChart = dc.pieChart("#region-pie-chart");
+    var agePieChart = dc.pieChart("#age-pie-chart");
+
+    var colorScale = d3.scale.ordinal().range(["#ffc0cb", "#ff6680", "#ff1a40", "#acbdec", "#2e59cd", "#121382", "#0c0e5a", "#ffd280","#ffc14d", "#ffa500", "#b37400"  ]);
+
+    // (["#b3b3b3", "#808080", "#595959", "#acbdec", "#2e59cd", "#121382", "#0c0e5a", "#ffd280","#ffc14d", "#ffa500", "#b37400"  ]);  ==== original color array
 
        YearChart
        .width(800)
-       .height(200)
+       .height(500) // was 400
        .margins({top: 10, right: 50, bottom: 30, left: 50})
        .dimension(YearDim) // was YearDim
-       .group(numYear)  // was numYear
+       // .group(numYear)  // was numYear
        .transitionDuration(500)
-       .x(d3.time.scale().domain([minYear, maxYear]))
-       .elasticY(true)
+       .x(d3.scale.linear().domain([2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014]))        // .x(d3.time.scale().domain([minYear, maxYear]))
+       .y(d3.scale.linear().domain([0, 100]))
        .xAxisLabel("Year")
+       .elasticX(true)
        .brushOn(false)
-       .yAxisLabel("Employment (%)")
-       .yAxis().ticks(4);
+       .yAxisLabel("Employment rate (in %)")
+       .compose([dc.lineChart(YearChart).group(numMhEmployed, "Employment rate of people with mental illness").colors('#2e59cd').valueAccessor(function(k) { return k.value.average }),dc.lineChart(YearChart).group(numPopEmployed, "Employment rate of general population").colors('#ffa500').valueAccessor(function(k) { return k.value.average })])
+       .legend(dc.legend().x(550).y(0).gap(5))
+       .yAxis().ticks(48);
 
         MhEmployedChart
-       .width(300)
+       .width(550)
        .height(250)
-       .dimension(mhEmployedDim) // was mhEmployedDim - experimenting
+       .dimension(YearDim) // was mhEmployedDim - experimenting
        .group(numMhEmployed)  // was numMhEmployed - works nicely on numYear
-       .xAxis().ticks(4);
+       // .x(d3.scale.linear().domain([0, 100]))
+       .colors(colorScale)
+       .valueAccessor(function(k) { return k.value.average })
+       .xAxis().ticks(8);
 
         PopEmployedChart
-       .width(300)
+       .width(550)
        .height(250)
-       .dimension(popEmployedDim)
+       .dimension(YearDim)
        .group(numPopEmployed)
-       .xAxis().ticks(4);
+       .colors(colorScale)
+       .valueAccessor(function(k) { return k.value.average })
+       .xAxis().ticks(8);
+
+        regionPieChart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(regionDim)     //was YearDim
+      // .group(numGender)
+       .group(numRegion)
+       .colors(colorScale)
+       .title(function(d){return d.value;});
+       // .valueAccessor(function(k) { return k.value.average });
+
+        agePieChart
+       .height(220)
+       .radius(90)
+       .innerRadius(40)
+       .transitionDuration(1500)
+       .dimension(ageDim)     //was YearDim
+      // .group(numGender)
+       .group(numAge)
+       .colors(colorScale)
+       .title(function(d){return d.value;});
+
+        // var x = '2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014';
+        // x.replace(',','');
 
     dc.renderAll();
 }
